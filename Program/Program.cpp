4a85f9f0443416ddbc3d397f28f1509671b8c818
thankyou1223 <thankyou7608@naver.com>
2024-12-11 15:44:40 +0900

    2024.12.11(양방향)

--- Program/Program.cpp
+++ Program/Program.cpp
@@ -1,9 +1,20 @@
 ﻿
 #include <iostream>
 
+using namespace std;
+
+template <typename T>
+class DoubleLinkedList
+{
+private;
+	
+};
 int main()
 {
-    
+	DoubleLinkedList<int> doubieLinkedList
+
+	return 0;
 }
+
 
 
