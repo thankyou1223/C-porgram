78053feaabab36c57559c9e61883a21fe4be2949
thankyou1223 <thankyou7608@naver.com>
2024-12-18 15:43:36 +0900

    2024.12.18(헤시 테이블)

--- class8th(string)/class8th(string).cpp
+++ class8th(string)/class8th(string).cpp
@@ -1,5 +1,166 @@
 ﻿// class8th(string).cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
-//
 
 #include <iostream>
 
+using namespace std;
+
+class String
+{
+private:
+	int size;
+	char* pointer;
+
+public:
+	String()
+	{
+		size = 0;
+		pointer = nullptr;
+	}
+
+	void operator = (const char* word)
+	{
+		size = strlen(word) + 1;
+
+		if (pointer == nullptr)
+		{
+			pointer = new char[size];
+
+			for (int i = 0; i < size; i++)
+			{
+				pointer[i] = word[i];
+			}
+		}
+		else
+		{
+			char* newPointer = new char[size];
+
+			for (int i = 0; i < size; i++)
+			{
+				newPointer[i] = word[i];
+			}
+
+			delete[] pointer;
+
+			pointer = newPointer;
+		}
+
+	}
+
+	void Append(const char* word)
+	{
+		size = strlen(pointer) + strlen(word) + 1;
+
+		char* newPointer = new char[size];
+
+		for (int i = 0; i < strlen(pointer); i++)
+		{
+			newPointer[i] = pointer[i];
+		}
+
+		for (int i = 0; i < strlen(word); i++)
+		{
+			newPointer[strlen(pointer) + i] = word[i];
+		}
+
+		delete[] pointer;
+
+		pointer = newPointer;
+
+	}
+
+	int Compare(const char* word)
+	{
+		int count = 0;
+
+		for (int i = 0; i < strlen(word); i++)
+		{
+			if (pointer[i] != word[i])
+			{
+				break;
+			}
+			else
+			{
+				count++;
+			}
+		}
+
+		if (strlen(word) == count)
+		{
+			return 0;
+		}
+
+		int thisString = 0;
+		int otherString = 0;
+
+		for (int i = 0; i < strlen(pointer); i++)
+		{
+			thisString += pointer[i];
+		}
+
+		for (int i = 0; i < strlen(word); i++)
+		{
+			otherString += word[i];
+		}
+
+		if (thisString > otherString)
+		{
+			return 1;
+		}
+		else
+		{
+			return -1;
+		}
+
+	}
+
+	const int& Size()
+	{
+		return size - 1;
+	}
+
+	const char& operator [] (const int& index)
+	{
+		return pointer[index];
+	}
+
+	~String()
+	{
+		if (pointer != nullptr)
+		{
+			delete[] pointer;
+		}
+	}
+
+};
+
+int main()
+{
+	String string;
+
+	string = "Apple";
+
+	for (int i = 0; i < string.Size(); i++)
+	{
+		cout << string[i];
+	}
+
+	string = "Banana";
+
+	string.Append("Milk");
+
+	cout << endl;
+
+	for (int i = 0; i < string.Size(); i++)
+	{
+		cout << string[i];
+	}
+
+	cout << endl;
+
+	string = "Lee";
+
+	cout << string.Compare("Lef");
+
+	return 0;
+
+}
---
+++ class9th(Hash Table)/class9th(Hash Table).cpp
@@ -0,0 +1,114 @@
+﻿// class9th(Hash Table).cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+
+#include <iostream>
+
+#define SIZE 6
+
+using namespace std;
+
+template <typename KEY, typename VALUE>
+class HashTable
+{
+private:
+	struct Node
+	{
+		KEY key;
+		VALUE value;
+
+		Node* next;
+	};
+
+	struct Bucket
+	{
+		int count;
+		Node* head;
+	};
+
+	Bucket bucket[SIZE];
+public:
+	HashTable()
+	{
+		for (int i = 0; i < SIZE; i++)
+		{
+			bucket[i].count = 0;
+			bucket[i].head = nullptr;
+		}
+	}
+
+	template<typename T>
+	int HashFunction(T key)
+	{
+		unsigned int hashIndex = (int)key % SIZE;
+
+		return hashIndex;
+	}
+
+	template<>
+	int HashFunction(const char* key)
+	{
+		int result = 0;
+
+		for (int i = 0; i < strlen(key); i++)
+		{
+			result += key[i];
+		}
+
+		unsigned int hashIndex = result % SIZE;
+
+		return hashIndex;
+	}
+
+	void Insert(KEY key, VALUE value)
+	{
+		// 해시 함수를 통해서 값을 받는 임시 변수
+		int hashIndex = HashFunction(key);
+
+		// 새로운 노드를 생성합니다.
+		Node* newNode = CreateNode(key, value);
+
+		// 노드가 1개라도 존재하지 않는다면
+		if (bucket[hashIndex].count == 0)
+		{
+			// bucket[hashIndex]의 head 포인터가 newNode를 가리키게 합니다.
+			bucket[hashIndex].head = newNode;
+		}
+		else
+		{
+			newNode->next = bucket[hashIndex].head;
+
+			bucket[hashIndex].head = newNode;
+		}
+
+		// bucket[hashIndex]의 count를 증가시킵니다.
+		bucket[hashIndex].count++;
+	}
+
+	Node* CreateNode(KEY key, VALUE value)
+	{
+		Node* newNode = new Node();
+
+		newNode->key = key;
+
+		newNode->value = value;
+
+		newNode->next = nullptr;
+
+		return newNode;
+	}
+};
+
+int main()
+{
+	HashTable<const char*, int> hashTable;
+
+	cout << hashTable.HashFunction("AWQfrwwfwf") << endl;
+
+	hashTable.Insert("Sword", 10000);
+	hashTable.Insert("Armor", 25000);
+	hashTable.Insert("Posion", 5000);
+
+	return 0;
+}
